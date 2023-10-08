4a2a710cc3459c5b45e9712e59bb5737d2acaaa1
Chayapol Wongpuwarak <kongkcypw@gmail.com>
2023-10-08 16:31:35 +0700

    re-factor: change systax for call route

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const PORT = 3000;
 const app = express();
+
+// PORT configuration
+const PORT = 3200;
 
 // Configuration for cors
 const corsOptions = {
-    origin: '*',
+    origin: '*', // Allow for all domains
     credentials: true,
 };
 app.use(cors(corsOptions));
 
+// BodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-require('./routes/df-routes')(app);
-
+// Dialogflow routes
+const dialogflowRoutes = require('./routes/df-routes');
+dialogflowRoutes(app);
 
 app.listen(PORT, () => console.log(`Server is now listening on port ${PORT}`));
