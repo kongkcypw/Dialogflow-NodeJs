cabead03e29f0d4a8383347d3d452c60153da3c1
Chayapol Wongpuwarak <kongkcypw@gmail.com>
2023-10-08 16:40:38 +0700

    delete routes, post method inside index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const chatbot = require('./chatbot/chatbot')
 
 // PORT configuration
 const PORT = 3200;
@@ -17,8 +18,18 @@ app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Dialogflow routes
-const dialogflowRoutes = require('./routes/df-routes');
-dialogflowRoutes(app);
+// Post request to endpoint
+app.post('/text_query', async(req, res) => {
+    console.log(req);
+    const {text, userId} = req.body;
+    const resultQuery = await chatbot.textQuery(text, userId);
+    console.log(resultQuery);
+    const resObj = {
+        intentName: resultQuery.intent.displayName,
+        userQuery: resultQuery.queryText,
+        fulfillmentText: resultQuery.fulfillmentText
+    }
+    res.send(resObj);
+})
 
 app.listen(PORT, () => console.log(`Server is now listening on port ${PORT}`));
