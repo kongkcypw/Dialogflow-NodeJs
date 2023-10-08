da11b94bb44028d182565487de3c84e3fa598173
Chayapol Wongpuwarak <kongkcypw@gmail.com>
2023-10-08 16:00:03 +0700

    add: cors config

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const PORT = 3000;
 const app = express();
+
+// Configuration for cors
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/test', (req, res) => {
-    res.send('Hi, from server');
-}); 
-
 require('./routes/df-routes')(app);
+
 
 app.listen(PORT, () => console.log(`Server is now listening on port ${PORT}`));
